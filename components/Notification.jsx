f5fe86b61325e65f6dcd3bca5186ca0bce971e76
Artem Veikus <artem@veikus.com>
2025-08-15 21:48:54 +0200

    Notification updated

--- components/Notification.jsx
+++ components/Notification.jsx
@@ -5,7 +5,7 @@ export default function Notification() {
   return (
     <div className={styles.notification}>
       <p>
-        📅 <strong>Site and timetable updated Jun 13, 2025.</strong>{' '}
+        📅 <strong>Timetable updated on Aug 15, 2025. And we moved to a more reliable server!</strong>{' '}
         Report about issues:&nbsp;
         <a href="mailto:artem@veikus.com">artem@veikus.com</a>
       </p>
